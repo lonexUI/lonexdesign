f23e28fa0be53258fa9cd45007effa4e8c4d2e83
Gjonson <gjonsonberisha@gmail.com>
2026-04-06 23:01:35 +0200

    cleaner css

--- lonex_ds/src/app/components/test_components/btn.tsx
+++ lonex_ds/src/app/components/test_components/btn.tsx
@@ -1,22 +1,25 @@
 "use client";
 
+import { style } from 'framer-motion/client';
 import styles from './components.module.css';
 interface BtnProps {
     variant?: 'primary' | 'secondary' | 'tretiary';
+    size?: 'sm' | 'md' | 'lg';
     disabled?: boolean;
     rounded?: boolean;
     label?: string;
 }
 
-const Btn: React.FC<BtnProps> = ({ variant = 'primary', rounded = false, disabled = false, label = 'Button' }) => {
-    let btnClass = variant === 'primary' ? styles.btnPrimary : variant === 'secondary' ? styles.btnSecondary : styles.btnTertiary;
+const Btn: React.FC<BtnProps> = ({ variant = 'primary', size = 'md', rounded = false, disabled = false, label = 'Button' }) => {
+    let btnClass = styles.btn + ' ' + (variant === 'primary' ? styles.btnPrimary : variant === 'secondary' ? styles.btnSecondary : styles.btnTertiary);
+    const btnSize = size === 'sm' ? styles.btnSm : size === 'lg' ? styles.btnLg : styles.btnMd;
     if (rounded) {
         btnClass += ` ${styles.btnRounded}`;
     }
 
 
     return (
-        <button className={btnClass} disabled={disabled}>
+        <button className={btnClass + ' ' + btnSize} disabled={disabled}>
             {label}
         </button>
     )
