2d5e2acfbd1dee63d9207c3a10f483024bfcaed3
Gjonson <gjonsonberisha@gmail.com>
2026-04-07 18:35:13 +0200

    added variant props for the alert component

--- lonex_ds/src/app/components/test_components/alert.tsx
+++ lonex_ds/src/app/components/test_components/alert.tsx
@@ -2,10 +2,15 @@
 
 import styles from './components.module.css';
 
-const Alert: React.FC = () => {
+interface AlertProps {
+    variant?: 'danger' | 'info' | 'success' | 'warning';
+}
+
+const Alert: React.FC<AlertProps> = ({ variant = 'warning' }) => {
+    const alertClass = styles.alert + " " + styles[`alert${variant.charAt(0).toUpperCase() + variant.slice(1)}`];
     return (
-        <div className={styles.alert + " " + styles.alertWarning}>
-            <strong>Warning!</strong> This is an alert message.
+        <div className={alertClass}>
+            <strong>{variant.charAt(0).toUpperCase() + variant.slice(1)}!</strong> This is an alert message.
         </div>
     )
 }
