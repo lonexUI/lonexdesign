5ba6f8844a3a3413d3e0d331e67fe89ae931a708
Gjonson <gjonsonberisha@gmail.com>
2026-04-09 22:29:22 +0200

    added styling to the cards, will have to add props too

--- lonex_ds/src/app/components/test_components/card.tsx
+++ lonex_ds/src/app/components/test_components/card.tsx
@@ -1,5 +1,15 @@
 "use client";
+import styles from './components.module.css';
+import Btn from './btn';
+import Alert from './alert';
 
 export default function Card() {
-    return <div style={{ padding: '1rem', backgroundColor: '#e0e0e0', border: '1px solid #aaa' }}>This is a Card component</div>;
+    return <div className={styles.card}>
+        {/* <div className={styles.fakeImage}></div> */}
+        <h3>Card Title</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
+        {/* name Btn will become ldsBtn in future */}
+        <Alert variant="info" />
+        <Btn variant="primary" label="Click Me" />
+    </div>;
 }
