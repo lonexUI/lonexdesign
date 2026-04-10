e8c9c74d481b40085ee455db5cc532435b375e40
Gjonson <gjonsonberisha@gmail.com>
2026-04-10 12:42:44 +0200

    added props for the card

--- lonex_ds/src/app/components/test_components/card.tsx
+++ lonex_ds/src/app/components/test_components/card.tsx
@@ -1,15 +1,22 @@
 "use client";
 import styles from './components.module.css';
 import Btn from './btn';
-import Alert from './alert';
+import { div, image } from 'framer-motion/client';
+// import Alert from './alert';
 
-export default function Card() {
+interface CardProps {
+    alert?: boolean;
+    image?: boolean;
+}
+
+export default function Card({ alert, image }: CardProps) {
     return <div className={styles.card}>
         {/* <div className={styles.fakeImage}></div> */}
+        {image && <div className={styles.fakeImage}></div>}
         <h3>Card Title</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
         {/* name Btn will become ldsBtn in future */}
-        <div className={styles.alertWarning + ' ' + styles.alertMini}>Dont do that!</div>
         <Btn variant="primary" label="Click Me" />
+        {alert && <div className={styles.alertWarning + ' ' + styles.alertMini}>Dont do that!</div>}
     </div>;
 }
